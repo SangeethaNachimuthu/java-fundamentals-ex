d392083ffdb1fea5b8c497ec88a3a343098881cb
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2025-11-25 09:36:04 +0100

    Add exercise 2 changes

--- src/main/java/Exercise.java
+++ src/main/java/Exercise.java
@@ -1,12 +1,31 @@
-public class Exercise {
+import java.util.Scanner;
 
+public class Exercise {
 
     public static void main(String[] args) {
 
         /*
-        Exercise 1: Print Hello and Name
+        Exercise 1: Print Hello and Name.
         * */
         System.out.println("Hello");
         System.out.println("Sangeetha!");
+
+        leapYear();    //Exercise 2
+
+
+    }
+    /*
+    Exercise 2: Get a year as input and print if is a leap year or not.
+     */
+    public static void leapYear(){
+         System.out.println("Enter the year: ");
+         Scanner scanner = new Scanner(System.in);
+         int year = scanner.nextInt();
+         if (year % 4 == 0){
+             System.out.println("The "+ year + " is a leap year.");
+         }
+         else {
+             System.out.println("The " + year + " is not a leap year.");
+         }
     }
 }
