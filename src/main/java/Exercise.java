2c002f2b5c19bb269405934ee0f349d444db4e1b
Sangeetha Nachimuthu <sangeen.muthu@gmail.com>
2025-11-25 09:17:42 +0100

    Initial commit

---
+++ src/main/java/Exercise.java
@@ -0,0 +1,12 @@
+public class Exercise {
+
+
+    public static void main(String[] args) {
+
+        /*
+        Exercise 1: Print Hello and Name
+        * */
+        System.out.println("Hello");
+        System.out.println("Sangeetha!");
+    }
+}
